63b405f6bff1ba0ac7ca952065b161f6dd45a17e
Fellipe Juncal <fjuncal7@gmail.com>
2021-08-04 00:21:34 -0300

    CORS configuration

--- clientes-vavive/src/main/java/br/com/vavive/clientes/rest/controllers/ServicoPrestadoController.java
+++ clientes-vavive/src/main/java/br/com/vavive/clientes/rest/controllers/ServicoPrestadoController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +24,6 @@ import br.com.vavive.clientes.util.BigDecimalConverter;
 
 @RestController
 @RequestMapping("api/servicos-prestados")
-@CrossOrigin("http://localhost:4200")
 public class ServicoPrestadoController {
 	
 	@Autowired
