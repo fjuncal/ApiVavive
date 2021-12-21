bbc3c8bf9afa22d98e6a5807772c01428aaa761e
Fellipe Juncal <fjuncal7@gmail.com>
2021-12-21 19:10:09 -0300

    Adicionando endpoint de importação de planilha no controller de profissionais

--- clientes-vavive/src/main/java/br/com/vavive/clientes/rest/controllers/ProfissionalController.java
+++ clientes-vavive/src/main/java/br/com/vavive/clientes/rest/controllers/ProfissionalController.java
@@ -1,5 +1,8 @@
 package br.com.vavive.clientes.rest.controllers;
 
+import static br.com.vavive.clientes.service.planilha.entity.TipoPlanilhaEnum.FUNCIONARIO;
+
+import java.io.IOException;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -13,12 +16,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import br.com.vavive.clientes.model.entity.Profissional;
 import br.com.vavive.clientes.model.repository.ProfissionalRepository;
+import br.com.vavive.clientes.rest.dto.ResultadoImportacaoDTO;
+import br.com.vavive.clientes.service.planilha.PlanilhaService;
 
 @RestController
 @RequestMapping("/profissionais")
@@ -26,6 +33,9 @@ public class ProfissionalController {
 	
 	@Autowired
 	private ProfissionalRepository repository;
+	
+	@Autowired
+	private PlanilhaService planilhaService;
 	
 	@GetMapping()
 	public List<Profissional> obterTodos(){
@@ -67,5 +77,11 @@ public class ProfissionalController {
 				return repository.save(profissionalAtualizado);
 			})
 			.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Profissional não encontrado") );
+	}
+	
+	@PostMapping("/importar")
+	public ResultadoImportacaoDTO importarPlanilha(@RequestParam("arquivo") MultipartFile file) throws IOException {
+		System.out.println("bateu aqui");
+		return planilhaService.importar(file, FUNCIONARIO);
 	}
 }
